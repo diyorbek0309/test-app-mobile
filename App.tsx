243de737a2b1587b8042669a98e6503ccdc18a48
Diyorbek Olimov <diyorbekolimov2000@gmail.com>
2022-09-29 00:01:12 +0500

    added routing and login&register screens

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/Home';
+import Login from './src/screens/Login';
+import Register from './src/screens/Register';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const { Navigator, Screen } = createNativeStackNavigator();
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigator>
+        <Screen name="Home" component={HomeScreen} />
+        <Screen name="Login" component={Login} />
+        <Screen name="Register" component={Register} />
+      </Navigator>
+    </NavigationContainer>
   );
 }
+
+export default App;
---
+++ src/screens/Register.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+function Register() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Register Screen</Text>
+    </View>
+  );
+}
+
+export default Register;
